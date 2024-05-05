a013898605a1de5ec3cb4e821e9efa60bcf1d5f2
ImGabreuw <imgabreuw.dev@gmail.com>
2024-05-05 03:25:15 -0300

    fix(back-end): add new providers

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {OpenaiModule} from './openai/openai.module';
+import {ImageProcessorModule} from './image-processor/image-processor.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({isGlobal: true}), OpenaiModule, ImageProcessorModule],
 })
-export class AppModule {}
+export class AppModule {
+}
