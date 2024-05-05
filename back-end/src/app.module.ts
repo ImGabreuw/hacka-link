7c791365e616eb8a578ca07f0bdaff6981821440
ImGabreuw <imgabreuw.dev@gmail.com>
2024-05-05 12:20:30 -0300

    test(back-end): add integration test for gen content

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -5,7 +5,7 @@ import {ConfigModule} from "@nestjs/config";
 import {UserProfileModule} from './user-profile/user-profile.module';
 import {FirebaseModule} from './firebase/firebase.module';
 import {GenContentModule} from './gen-content/gen-content.module';
-import { ContentModule } from './content/content.module';
+import {ContentModule} from './content/content.module';
 
 @Module({
     imports: [ConfigModule.forRoot({isGlobal: true}), OpenaiModule, ImageProcessorModule, UserProfileModule, FirebaseModule, GenContentModule, ContentModule],
--- back-end/src/gen-content/gen-content.service.spec.ts
+++ back-end/src/gen-content/gen-content.service.spec.ts
@@ -1,18 +1,37 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { GenContentService } from './gen-content.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {GenContentService} from './gen-content.service';
+import {FirebaseModule} from "../firebase/firebase.module";
+import {UserProfileService} from "../user-profile/user-profile.service";
+import {OpenaiService} from "../openai/openai.service";
+import {ConfigService} from "@nestjs/config";
+import {ImageProcessorService} from "../image-processor/image-processor.service";
+import {ContentService} from "../content/content.service";
 
 describe('GenContentService', () => {
-  let service: GenContentService;
+    let service: GenContentService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [GenContentService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [FirebaseModule],
+            providers: [ConfigService, ImageProcessorService, GenContentService, UserProfileService, OpenaiService, ContentService],
+        }).compile();
 
-    service = module.get<GenContentService>(GenContentService);
-  });
+        service = module.get<GenContentService>(GenContentService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should generate text content', async () => {
+        const content = await service.text_generation({
+          username: "Gabriel",
+          text: "Faça uma introdução sobre classes em Python."
+        });
+
+        console.log(content);
+
+        expect(content).not.toBeNull();
+        expect(content.trim().length).toBe(0);
+    });
 });
